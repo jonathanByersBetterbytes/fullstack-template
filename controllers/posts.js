51a53da4cdb06a619b2646cba5d32118da7128fb
JP <jonathankbyers@gmail.com>
2024-01-23 15:26:01 -0500

    make a reciept app

--- controllers/posts.js
+++ controllers/posts.js
@@ -10,7 +10,7 @@ module.exports = {
       //Grabbing just the posts of the logged-in user
       const posts = await Post.find({ user: req.user.id });
       //Sending post data from mongodb and user data to ejs template
-      res.render("profile.ejs", { posts: posts, user: req.user });
+      res.render("profile.ejs", { posts: posts, user: req.user }); 
     } catch (err) {
       console.log(err);
     }
@@ -19,10 +19,19 @@ module.exports = {
     try {
       //id parameter comes from the post routes
       //router.get("/:id", ensureAuth, postsController.getPost);
-      //http://localhost:2121/post/631a7f59a3e56acfc7da286f
+      //http://localhost:3000/post/631a7f59a3e56acfc7da286f
       //id === 631a7f59a3e56acfc7da286f
       const post = await Post.findById(req.params.id);
       res.render("post.ejs", { post: post, user: req.user});
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getFeed: async (req, res) => {
+    try {
+      //get all posts in desc order by createdAt
+      const posts = await Post.find().sort({ createdAt: "desc" }).lean()
+      res.render("feed.ejs", { posts: posts });
     } catch (err) {
       console.log(err);
     }
